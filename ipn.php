6e881855badab9c2e67bfe6e08c00767d51bd604
andy-walker <andyw+github2@circle-interactive.co.uk>
2015-11-06 17:45:27 +0000

    Updated licence, authorship information

--- ipn.php
+++ ipn.php
@@ -1,12 +1,24 @@
 <?php
 
-/* 
- * Worldpay Extension for CiviCRM - Circle Interactive 2015
- * Author: Drew Webber (mcdruid), andyw@circle
- * Callback Notification Class
+/**
+ * Worldpay Extension for CiviCRM - Callback Notification class
+ * Original code by GreenNet Ltd
+ * @author idmacdonald (imac@gn.apc.org) - https://github.com/idmacdonald/civicrm-worldpay
+ * Extension-ized / maintained by Circle Interactive
  *
- * Distributed under GPL v2
- * https://www.gnu.org/licenses/gpl-2.0.html
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 require_once 'CRM/Core/Payment/BaseIPN.php';
